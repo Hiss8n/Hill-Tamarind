3591c1e898bb1b1ef353bc213e5f17ee84c333e9
Hissen Tutu <hissentutu@gmail.com>
2025-06-17 00:55:49 -0700

    homapage dashboard

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -31,8 +31,8 @@ function App() {
   <Route path='/about' element={<AboutPage/>}/>
   <Route path='/projects' element={<Projects/>}/>
   <Route path='/contact' element={<Contact/>}/>
-<Route path='*' element={<NotFound/>}/>
   <Route path='*' element={<NotFound/>}/>
+ 
   
 </Routes>
 
